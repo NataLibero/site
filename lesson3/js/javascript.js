6ecb5188ba6ce9a3ce81c8f73a0abf5d0feae137
Natalia <jakushkolibero@gmail.com>
2018-06-01 17:13:06 +0200

    Third lesson

---
+++ lesson3/js/javascript.js
@@ -0,0 +1,89 @@
+var cost;
+var shopTitle;
+var price;
+
+function start() {
+	cost = prompt("Ваш бюджет на месяц?", "");
+
+	while(isNaN(cost) || cost == null || cost == "") {
+		cost = prompt("Ваш бюджет на месяц?", "");
+	}
+
+	shopTitle = prompt("Название вашего магазина?", "").toUpperCase();
+}
+
+start();
+
+var mainList = {
+	'budget': cost,
+	'shopTitle': shopTitle,	
+	'shopGoods': [],
+	'employers': {},
+	'open': true,
+	'discount': false
+}
+
+function chooseGoods() {
+	for (var i = 0; i < 3; i++) {
+		var answer = prompt("Какой тип товаров будем продавать?", "");
+		if ((typeof(answer)) === 'string' && answer.length < 50 && answer != '' && answer !== null) {
+			console.log('все верно');
+			mainList['shopGoods'][i] = answer;
+		} else {
+			console.log('ввод некорректный');
+			i--;
+		}
+	}
+}
+
+chooseGoods();
+
+function workTime(time) {
+	if (time < 0) {
+		console.log('не может быть');
+	} else if (time > 8 && time < 20) {
+		console.log('Время работать');
+	} else if (time < 24) {
+		console.log('Уже слишком поздно');
+	} else {
+		console.log('В сутках только 24 часа');
+	}
+}
+
+workTime(18);
+
+function getBudget() {
+	alert( 'Бюджет за 1 день: ' + 30 / cost );
+}
+
+getBudget();
+
+function getDiscount(discount) {
+	price = prompt("Укажите цену", "");
+	if(discount == true) {
+		price = price / 100 * 80;
+	}
+	return price;
+}
+
+getDiscount(true);
+
+console.log(price);
+
+function getEmployers() {
+	for(var j = 1; j <= 4; j++) {
+		var name = prompt('Введите имя сотрудника', '');
+		if((typeof(name)) == 'string' && (typeof(name)) !== null && name != "" && name.length < 20) {
+			mainList['employers'][j] = j + '-' + name;
+		} else {
+			console.log('Неправильный ввод');
+			j--;
+		}
+
+	}
+}
+
+getEmployers();
+
+console.log(mainList);
+
---
+++ lesson3/js/script2.js
@@ -0,0 +1,47 @@
+var str = "урок-3-был слишком легким";
+var str2 = str.slice(0, 1).toUpperCase() + str.slice(1);
+console.log(str2);
+
+
+var arr = str2.split('-');
+var arr2 = [];
+
+for (var i = 0; i < arr.length; i++) {
+	arr2[i] = arr[i];
+}
+
+var strn = arr2.join(' ');
+console.log(strn);
+
+var word = strn.slice(-6);
+var wordArr = word.split('');
+wordArr.splice(4, 2, "o", "o");
+var wordStr = wordArr.join('');
+console.log(wordStr);
+
+
+var arrlast = [20, 33, 1, "Человек", 2, 3];
+var sum = 0;
+for (var j = 0; j < arrlast.length; j++) {
+	if(isNaN(arrlast[j])) {
+		continue;
+	} else {
+		sum += Math.pow(arrlast[j], 3);
+	}	
+}
+console.log(Math.sqrt(sum));
+
+function checkString(result) {
+	if((typeof(result)) != 'string') {
+		alert('это не строка')
+	} else {
+		result = result.replace(/^\s+|\s+$/g, "");
+		if (result.length > 50) {
+			result = result.slice(0, 50 - 3) + '...';    
+			console.log(result);    
+		}		
+	}
+}
+
+checkString(' Текст внутри пробелов Текст внутри пробелов Текст внутри пробелов Текст внутри пробелов Текст внутри пробелов Текст внутри пробелов Текст внутри пробелов ');
+
