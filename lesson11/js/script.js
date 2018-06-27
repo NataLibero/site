d6b13404cb2639a4b729031d0fb0a291f39880da
Natalia <jakushkolibero@gmail.com>
2018-06-27 17:10:35 +0200

    correct ajax form

--- lesson11/js/script.js
+++ lesson11/js/script.js
@@ -131,8 +131,68 @@ close.addEventListener('click', () => {
 });	
 
 
-// form
+//form
 
+let message = new Object();
+message.loading = "Загрузка...";
+message.success = "Спасибо! Скоро мы с вами свяжемся";
+message.failure = "Что-то пошло не так...";
+
+let form = document.getElementsByClassName('main-form')[0],
+formBottom = document.getElementById('form'),
+input = document.getElementsByTagName('input'),
+statusMessage = document.createElement('div');
+statusMessage.classList.add('status');
+
+function sendForm(elem) {
+	elem.addEventListener('submit', function(e) {
+		e.preventDefault();
+		elem.appendChild(statusMessage);
+		let formData = new FormData(elem);
+
+		function postData(data) {
+			return new Promise(function(resolve, reject) {
+				let request = new XMLHttpRequest();
+				request.open('POST', 'server.php');
+				request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				request.onreadystatechange = function() {
+					if (request.readyState < 4) {
+						resolve()
+					} else if(request.readyState === 4) {
+						if(request.status == 200 && request.status < 300) {
+							resolve()
+						} else {
+							reject()
+						}
+					}
+				}
+				request.send(data);
+			}) 
+			} // postData
+
+			function clearInput() {
+				for(let i = 0; i < input.length; i++) {
+					input[i].value = "";
+				}
+			}
+
+			postData(formData)
+			.then(() => statusMessage.innerHTML = message.loading)
+			.then(() => statusMessage.innerHTML = message.success)
+			.catch(() => statusMessage.innerHTML = message.failure)
+			.then(clearInput)
+		});
+}
+sendForm(form);
+sendForm(formBottom);
+
+
+
+
+
+
+// form
+/*
 let message = new Object();
 message.loading = "Загрузка...";
 message.success = "Спасибо! Скоро мы с вами свяжемся";
@@ -207,7 +267,7 @@ let formContact = document.getElementById("form"),
 			inputContact[i].value = '';			
 		}
 
-	})
+	})*/
 
 
 
